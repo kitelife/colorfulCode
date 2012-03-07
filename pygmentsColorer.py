f7e49f1036e94b4cbd3b38620d84d36070e3efec
xiayf <sas.198708@gmail.com>
2012-03-07 17:27:21 +0800

    modify pygmentsColorer.py

--- pygmentsColorer.py
+++ pygmentsColorer.py
@@ -18,15 +18,11 @@ def code2html(code, lang):
 
 def output_head():
     return  '''
-<!DOCTYPE html
-PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <title>O_O</title>
-        <!--<link rel="stylesheet" type="text/css" href="http://www.peerat.com/code/style.css" />-->
     </head>
     <body>
     '''
