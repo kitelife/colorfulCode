424a3dff5ccae863fcb584b487d216977063afed
xiayf <sas.198708@gmail.com>
2012-03-07 17:51:29 +0800

    use the pygments APIs get_all_lexers and get_all_styles

--- cc.py
+++ cc.py
@@ -2,15 +2,17 @@ from bottle import get,post,request,template,run
 import pygmentsColorer
 
 langlist = pygmentsColorer.getLexers()
+stylelist = pygmentsColorer.getStyles()
 
 @get('/')
 def getindex():
-	return template('index', langtypes=langlist);
+	return template('index', langtypes=langlist,styles=stylelist);
 
 @post('/')
 def postindex():
 	code = request.forms.get('code')
 	lang = request.forms.get('lang')
-	return pygmentsColorer.colorCode(code, lang)
+	style = request.forms.get('style')
+	return pygmentsColorer.colorCode(code, lang, style)
 
 run(host='192.168.1.32', port=8080)
--- pygmentsColorer.py
+++ pygmentsColorer.py
@@ -1,28 +1,39 @@
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name, get_all_lexers
 from pygments.formatters import HtmlFormatter
+from pygments.styles import get_all_styles
 
 def getLexers():
 	lexerlist =  [lexer[1][0] for lexer in get_all_lexers()]
 	lexerlist.sort()
 	return lexerlist
 
-def code2html(code, lang):
+def getStyles():
+	stylelist =  [style for style in get_all_styles()]
+	stylelist.sort()
+	return stylelist
+
+def code2html(code, lang, style):
 	lexer = get_lexer_by_name(lang, encoding='utf-8', stripall=True)
 	formatter = HtmlFormatter(
 			linenos=True,
 			encoding='utf-8',
-			noclasses='True'
+			noclasses='True',
+			style=style
 			)
 	return highlight(code, lexer, formatter)
 
 def output_head():
     return  '''
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <title>O_O</title>
+        <!--<link rel="stylesheet" type="text/css" href="http://www.peerat.com/code/style.css" />-->
     </head>
     <body>
     '''
@@ -30,5 +41,5 @@ def output_head():
 def output_end():
     return '</body>'+'</html>'
 
-def colorCode(code, lang):
-	return output_head()+code2html(code,lang)+output_end()
+def colorCode(code, lang, style):
+	return output_head()+code2html(code,lang,style)+output_end()
