e14a464b921be4385a22eccec2dc2e46eb37b967
xiayf <sas.198708@gmail.com>
2012-03-13 18:15:38 +0800

    in run function, add args: debug=True and reload=True

--- cc.py
+++ cc.py
@@ -51,4 +51,30 @@ def server_static(filename):
 	path=os.getcwd()+'/static'
 	return static_file(filename,root=os.getcwd()+'/static')
 
-run(host='192.168.1.32', port=8080)
+# debug=True
+# In debug mode, Bottle is much more verbose and provides helpful debugging information
+# whenever an error occurs. It also disables optimisations that might get in your way
+# Here is an incomplete list of things that change in debug mode:
+#        The default error page shows a traceback.
+#        Templates are not cached
+#        Plugins are applied immediately
+#
+# Just make sure to not use the debug mode on a production server.
+#
+# Bottle runs on the built-in wsgiref WSGIServer by default. This
+# non-threading HTTP server is perfectly fine for development and
+# early production, but may become a performance bottleneck when 
+# server load increases.
+# There are three ways to eliminate this bottleneck:
+#	Use a multi-threaded or asynchronous HTTP server
+#	Spread the load between multiple Bottle instances.
+#	Do both.
+#
+# reloader=True
+# During development, you have to restart the server 
+# a lot to test your recent changes. The auto reloader
+# can do this for you. Every time you edit a module file,
+# the reloader restarts the server process and loads the
+# newest version of your code.
+#
+run(host='127.0.0.1', port=8080, debug=True, reloader=True)
