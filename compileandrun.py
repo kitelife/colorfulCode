60eb28f9148fa442010cd6226ebe593a8dd33dd6
xiayf <sas.198708@gmail.com>
2012-03-14 18:19:02 +0800

    ...

--- compileandrun.py
+++ compileandrun.py
@@ -11,6 +11,7 @@ def _subprocessPopen(cmd,stdin=subprocess.PIPE,stdout=subprocess.PIPE,\
 
 def _scriptrun(cmd, filename):
 	p = _subprocessPopen(cmd)
+	p.stdin.write(" \n")
 	result =  p.stdout.read()+p.stderr.read()
 	os.remove(filename)
 	return result.replace('\n','<br />')
