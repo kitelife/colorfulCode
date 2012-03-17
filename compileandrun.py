34b475e42ff84eaffe247d24821db30b04518241
xiayf <sas.198708@gmail.com>
2012-03-18 02:49:03 +0800

    modify the process of compile(run) to multi-thread

--- compileandrun.py
+++ compileandrun.py
@@ -1,25 +1,72 @@
 import os
 import subprocess
 import time
+import threading
 
 _cwd = os.getcwd() + '/compileenv/'
 
+class Command(object):
+
+	def __init__(self, cmd):
+		self.cmd = cmd
+		self.process = None
+		self.out = None
+		self.err = None
+		self.returncode = None
+		self.data = None
+
+	def run(self, data, timeout, env):
+		self.data = data
+		environ = dict(os.environ).update(env or {})
+
+		def target():
+			self.process = subprocess.Popen(self.cmd,
+					shell=True,
+					env=environ,
+					stdin=subprocess.PIPE,
+					stdout=subprocess.PIPE,
+					stderr=subprocess.PIPE,
+					cwd=_cwd,
+					bufsize=0,
+			)
+
+			self.out, self.err = self.process.communicate(self.data)
+		thread = threading.Thread(target=target)
+		thread.start()
+
+		thread.join(timeout)
+		if thread.is_alive():
+			self.process.terminate()
+			thread.join()
+		self.returncode = self.process.returncode
+		return self.out, self.err
+'''
 def _subprocessPopen(cmd,stdin=subprocess.PIPE,stdout=subprocess.PIPE,\
 		stderr=subprocess.PIPE,cwd=_cwd,shell=True):
 	return subprocess.Popen(cmd,stdin=stdin,stdout=stdout,\
 			stderr=stderr,cwd=cwd,shell=shell)
-
+'''
 def _scriptrun(cmd, filename):
-	p = _subprocessPopen(cmd)
-	result =  p.stdout.read()+p.stderr.read()
+	#p = _subprocessPopen(cmd)
+	#result =  p.stdout.read()+p.stderr.read()
+	cmdprocess = Command(cmd)
+	cmdout, cmderr = cmdprocess.run(data=None,timeout=5000, env=None)
 	os.remove(filename)
+
+	result = cmdout + cmderr
+
 	return result.replace('\n','<br />')
 
 def _compilerun(compilecmd, runcmd, filename):
-	p = _subprocessPopen(compilecmd)
-	compileresult=p.stdout.read()+p.stderr.read()
-	p = _subprocessPopen(runcmd)
-	runresult = p.stdout.read()+p.stderr.read()
+	#p = _subprocessPopen(compilecmd)
+	#compileresult=p.stdout.read()+p.stderr.read()
+	compiling = Command(compilecmd)
+	compileout, compileerr = compiling.run(data=None, timeout=None, env=None)
+	compileresult = compileout +  compileerr
+
+	cmdprocess = Command(runcmd)
+	cmdout, cmderr = cmdprocess.run(data=None, timeout=5000, env=None)
+	runresult = cmdout + cmderr
 	return ('<b>Compile Output:</b><br />'+compileresult+\
 			'<br /><b>Run Output:</b><br />' + runresult).\
 			replace('\n','<br />')
@@ -44,7 +91,7 @@ def compilerunCode(sourcecode, lang):
 		if lang == 'c':
 			sourcefilename = filenamenosubfix+'.c'
 			open(sourcefilename, 'w').write(sourcecode)
-			compilecmd = 'gcc -Wall %s -o %s'%(sourcefilename, filenamenosubfix)
+			compilecmd = 'gcc -Wall -lm %s -o %s'%(sourcefilename, filenamenosubfix)
 			#runcmd = './%s'%filenamenosubfix
 			runcmd = './%s'%filenamenosubfix
 			result = _compilerun(compilecmd, runcmd, filenamenosubfix)
@@ -58,7 +105,7 @@ def compilerunCode(sourcecode, lang):
 		elif lang == 'c++':
 			sourcefilename = filenamenosubfix + '.cpp'
 			open(sourcefilename, 'w').write(sourcecode)
-			compilecmd = "g++ -Wall %s -o %s"%(sourcefilename, filenamenosubfix)
+			compilecmd = "g++ -Wall -lm %s -o %s"%(sourcefilename, filenamenosubfix)
 			#runcmd = './%s'%filenamenosubfix
 			runcmd = './%s'%(filenamenosubfix)
 			result =  _compilerun(compilecmd, runcmd, filenamenosubfix)
